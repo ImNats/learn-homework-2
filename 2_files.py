2dcb3c0adf78c1bb934c0aff61b1d9a8200cbfd5
Butova Anastasiia <as444541019@gmail.com>
2022-09-23 18:37:17 +0300

    выполнено ДЗ: 2_files.py
    проверочные текстовые файлы приложены

--- 2_files.py
+++ 2_files.py
@@ -11,12 +11,26 @@
 5. Сохраните результат в файл referat2.txt
 """
 
+
 def main():
-    """
-    Эта функция вызывается автоматически при запуске скрипта в консоли
-    В ней надо заменить pass на ваш код
-    """
-    pass
+    #  подготовить файл к записи
+    with open('referat2.txt', 'w', encoding='utf-8') as file_new:
+        file_new.close
+
+    with open('referat.txt', 'r', encoding='utf-8') as file_main:
+        line_count = 0
+        word_count = 0
+        for line in file_main:
+            line_count += 1
+            word_count += len(line.split())
+
+            new_line = line.replace(".", "!")
+            with open('referat2.txt', 'a', encoding='utf-8') as file_new:
+                file_new.write(f'{new_line}')
+
+    print(f'Количество строк в файле: {line_count}')
+    print(f'Количество слов в файле: {word_count}')
+
 
 if __name__ == "__main__":
     main()
